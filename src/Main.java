86a78f08a282e161e536ef61ce7d9ba53471a7c9
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-27 20:51:44 +0300

    dobavleho opisanie

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,11 @@ public class Main {
         int replenishment = 2200; // сумма пополнения
 
         int bonus;
-        if (replenishment > 1000) {
-            bonus = replenishment / 100;
+        if (replenishment > 1000) { // оператор для проверки суммы пополнения
+            bonus = replenishment / 100; //расчет суммы бонуса
         } else {
             bonus = 0;
         }
-        System.out.println("Итоговая сумма:" + bonus);
+        System.out.println("Итоговая сумма:" + bonus); //вывод на экран
     }
 }
